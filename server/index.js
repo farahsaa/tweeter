da4c3d10080e8c550e08d4351279370909db4bea
Sarah Farah <farahsaa@gmail.com>
2019-03-22 20:59:14 +0000

    created tweets using mongo

--- server/index.js
+++ server/index.js
@@ -6,19 +6,31 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-const DataHelpers = require("./lib/data-helpers.js")(db);
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+      console.error(`Failed to connect: ${MONGODB_URI}`);
+      throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  app.use("/tweets", tweetsRoutes);
+  // We have a connection to the "tweeter" db, starting here.
+});
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,9 +9,11 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
+      db.collection("tweets").insertOne(newTweet, (err) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, newTweet);
       });
     },
 
